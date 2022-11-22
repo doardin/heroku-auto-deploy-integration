49677a3f8de05c463accbcdcf0a283ee2b0eae5f
Eduardo Lopes <eduardo.santos@hexalab.com.br>
2022-11-22 14:45:10 -0300

    feat: Set callback url

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,25 @@
+import axios from "axios";
 import { Router } from "express";
 
 const routes = Router();
+let callbackUrl = "";
+routes.post("/set-callback-url", (request, response) => {
+    callbackUrl = request.body.url;
+    return response.json({
+        status: "Success"
+    })
+});
+
+routes.post("/", async (request, response) => {
+    const res = await axios
+        .create()
+        .post(callbackUrl, request.body);
+    console.log("Request response: " + res)
+    return response.send();
+});
 
 routes.get("/", (request, response) => {
-    return response.json({ message: "Hello world!" });
+    return response.send(request.query['hub.challenge']);
 });
 
 export default routes;
