cd6f56339a06c0c4e7eb68aa8daf91049c704dd7
Eduardo Lopes <eduardo.santos@hexalab.com.br>
2022-11-22 13:19:49 -0300

    fix: Reset commit

--- build/shared/http/routes/index.js
+++ build/shared/http/routes/index.js
@@ -3,6 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = require("express");
 var routes = (0, express_1.Router)();
 routes.get("/", function (request, response) {
-    return response.json({ message: "Hello world" });
+    return response.json({ message: "Hello world!" });
 });
 exports.default = routes;
