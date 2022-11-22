dea9b86849dc329112e16f4adf325a93bd821cf9
Eduardo Lopes <65371964+doardin@users.noreply.github.com>
2022-11-22 15:54:34 +0000

    fix: Added build to gitignore

--- build/shared/http/routes/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var routes = (0, express_1.Router)();
-routes.get("/", function (request, response) {
-    return response.json({ message: "Hello world" });
-});
-exports.default = routes;
