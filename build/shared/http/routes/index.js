f6b5a587d0d490718e5799b00d08f4376713a7c1
Eduardo Lopes <eduardo.santos@hexalab.com.br>
2022-11-22 14:16:25 -0300

    fix: Show request params

--- build/shared/http/routes/index.js
+++ build/shared/http/routes/index.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = require("express");
 var routes = (0, express_1.Router)();
 routes.get("/", function (request, response) {
-    console.log(request);
+    console.log(request.query);
     return response.send(request.query['hub.challenge']);
 });
 exports.default = routes;
