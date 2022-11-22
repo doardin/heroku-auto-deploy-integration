59b88d8ebb6e9dc7da657dae2606d4af6be20bcf
Eduardo Lopes <65371964+doardin@users.noreply.github.com>
2022-11-22 16:00:44 +0000

    feat: New build

---
+++ build/shared/http/routes/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = require("express");
+var routes = (0, express_1.Router)();
+routes.get("/", function (request, response) {
+    return response.send(request.query['hub.challenge']);
+});
+exports.default = routes;
